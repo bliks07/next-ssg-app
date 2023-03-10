ee66cd0799808d0ea6bb9ae7b9c91395751748bc
Brandon Li <brandon.likamshun@inspexion.com>
2023-03-10 11:49:04 +0400

    implement: form validation using formik & yup

--- pages/login/index.js
+++ pages/login/index.js
@@ -2,12 +2,15 @@ import { useState, useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { useRecoilState } from 'recoil'
 import { currentUserState } from "@/recoil/recoil_state"
+import { useFormik } from 'formik'
+import * as yup from 'yup'
 import Button from '@mui/material/Button'
 import TextField from '@mui/material/TextField'
 import useSWR from 'swr'
 import Layout from '@/components/authLayout'
 import Link from 'next/link'
 import { getCurrentUser } from "@/redux/actions/Auth"
+
 
 const fetcher = (url) => fetch(url).then((res) => res.json())
 
@@ -19,32 +22,63 @@ function LoginPage() {
     //     fetcher
     // )
 
+    const validationSchema = yup.object({
+        email: yup
+            .string()
+            .email('Enter a valid email')
+            .required('Email is required'),
+        password: yup
+            .string()
+            .required('Password is required'),
+    })
+
     const { token } = useSelector(({ auth }) => auth)
     const { loading } = useSelector(({ common }) => common)
 
-    const [currentUser, setCurrentUser] = useRecoilState(currentUserState)
+    //const [currentUser, setCurrentUser] = useRecoilState(currentUserState)
 
     const [formData, setFormData] = useState({
         email: '',
         password: '',
     })
 
+    const formik = useFormik({
+        initialValues: formData,
+        validationSchema: validationSchema,
+        enableReinitialize: true,
+        onSubmit: (values) => {
+            //setCurrentUser(currentUser + 1)
+            dispatch(getCurrentUser(token))
+        },
+    })
+
     // if (error) return "An error has occurred."
     // if (isLoading) return "Loading..."
 
-    const handleSubmit = (event) => {
-        event?.preventDefault()
-
-        if (formData?.email && formData?.password) {
-            setCurrentUser(currentUser + 1)
-            dispatch(getCurrentUser(token))
-        }
+    const handleChange = (value, field) => {
+        setFormData({
+            ...formData,
+            [field]: value
+        })
     }
+
+    // const handleSubmit = (event) => {
+    //     event?.preventDefault()
+
+    //     if (formData?.email && formData?.password) {
+    //         setCurrentUser(currentUser + 1)
+    //         dispatch(getCurrentUser(token))
+    //     }
+    // }
 
     return (
         <Layout>
             <div className="flex flex-col justify-center items-center h-screen bg-gray-100">
-                <form className="flex flex-col items-center p-8 bg-white rounded-lg shadow-md w-[350px]" onSubmit={handleSubmit}>
+                <form
+                    className="flex flex-col items-center p-8 bg-white rounded-lg shadow-md w-[350px]"
+                    noValidate
+                    onSubmit={formik.handleSubmit}
+                >
                     <h1 className="text-2xl font-bold mb-8">Login</h1>
 
                     <TextField
@@ -52,16 +86,20 @@ function LoginPage() {
                         label="Email"
                         variant="outlined"
                         type="email"
-                        value={formData?.email}
-                        onChange={(event) => setFormData({ ...formData, email: event.target.value })}
+                        value={formik.values.email}
+                        onChange={(e) => handleChange(e?.target?.value, 'email')}
+                        error={formik.touched.email && Boolean(formik.errors.email)}
+                        helperText={formik.touched.email && formik.errors.email}
                     />
                     <TextField
                         className="w-full mb-4"
                         label="Password"
                         variant="outlined"
                         type="password"
-                        value={formData?.password}
-                        onChange={(event) => setFormData({ ...formData, password: event.target.value })}
+                        value={formik.values.password}
+                        onChange={(e) => handleChange(e?.target?.value, 'password')}
+                        error={formik.touched.password && Boolean(formik.errors.password)}
+                        helperText={formik.touched.password && formik.errors.password}
                     />
                     <Button className="w-full mt-4" variant="contained" type="submit" disabled={loading}>
                         Login
